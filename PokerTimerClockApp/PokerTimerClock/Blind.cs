755fdcdb7fd6d34132a3310f0e1cdcadb03608cf
omarsotoanaya <omar.soto@softtek.com>
2017-03-10 16:45:18 -0800

    Agregar un XML para leer la configuracion del timer

---
+++ PokerTimerClockApp/PokerTimerClock/Blind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerTimerClock
+{
+
+    public class Blind
+    {       
+        public string SmallBlind { get; set; }
+        public string BigBlind { get; set; }
+        public string RoundNumber { get; set; }
+    }
+    
+}
